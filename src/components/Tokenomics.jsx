931c5d61a342f2d6cfe7166a0b921da0af82bae4
KoichiDevs <125679139+KoichiDevs@users.noreply.github.com>
2023-02-19 03:29:52 +0800

    Update Tokenomics.jsx

--- src/components/Tokenomics.jsx
+++ src/components/Tokenomics.jsx
@@ -58,7 +58,7 @@ const Tokenomics = () => {
             },
             title: {
                 display: true,
-                text: 'ALLOCATION',
+                text: 'TOKEN ALLOCATION',
                 color: '#FFFFFF',
             },
             tooltip: {
@@ -75,30 +75,10 @@ const Tokenomics = () => {
     return (
         <>
             <section className='w-full h-auto bg-tokenbg md:px-20 px-10 pt-28  text-white relative bg-token bg-cover bg-center' id="tokenomics">
-                <div className='lg:flex-row flex-col flex items-center justify-between'>
+                <div className='lg:flex-row flex-col flex justify-between'>
                     <div className='w-full h-auto mx-auto relative z-10'>
                         <h1 className='font-saira md:text-5xl 2xl:text-7xl font-bold text-white text-3xl'>TOKENOMICS</h1>
-                        <div className='mt-8 flex flex-col gap-y-4 font-poppins 2xl:text-lg'>
-                            <p><span className='text-2xl text-header font-saira font-bold 2xl:text-3xl'>1% </span> Further development of WebAI ecosystem</p>
-                            <p><span className='text-2xl text-header font-saira font-bold 2xl:text-3xl'>2% </span> Strategic marketing of WebAI token and ecosystem</p>
-                        </div>
-                    </div>
-
-                    <div className='w-full h-auto flex items-center lg:justify-center z-10 relative mt-10 lg:mt-0'>
-
-                        <div className='w-fit h-auto font-saira text-7xl 2xl:text-9xl'>
-                            <h1 className='text-header font-bold lg:text-center'>3%</h1>
-                            <h2 className='text-center mt-2 font-bold text-3xl 2xl:text-4xl opacity-60'>TOTAL TAX</h2>
-                        </div>
-
-                    </div>
-                </div>
-
-
-                <motion.img initial={{ scale: 0.5, opacity: 0 }} animate={inView ? { scale: 1, opacity: 100, y: [120, 0] } : {}} transition={{ duration: 0.7 }} src="/man.webp" alt="man" className=" absolute bottom-0 right-0 z-0 md:block hidden" ref={ref} />
-                <img src="/token.webp" alt="token" className=" absolute left-0 w-[35vh] my-auto top-0 bottom-0 ml-2" />
-
-                <section className='w-full h-auto py-20 pb-[4rem] relative z-10'>
+                        <section className='w-full h-auto py-20 pb-[4rem] relative z-10'>
 
 
                     <div className='text-left lg:text-lg md:text-md text-sm font-semibold font-saira mb-5 lg:mb-0'>
@@ -139,13 +119,31 @@ const Tokenomics = () => {
 
                     </div>
 
+                </section>
+                    </div>
 
-                    <div className='max-h-[25rem] flex items-center justify-between mx-auto lg:flex-row flex-col gap-y-10'>
+                    <div className='w-full h-auto flex items-top lg:justify-center z-10 relative mt-10 lg:mt-0'>
+
+                        <div className='w-fit h-auto font-saira text-2xl 2xl:text-9xltop'>
+                            <h1 className='text-center font-bold md:text-5xl 2xl:text-7xl'>TOTAL TAX <span className='text-header font-bold lg:text-center'>3%</span></h1>
+                            <div className='mt-8 flex flex-col gap-y-4 font-poppins 2xl:text-lg ml-10'>
+                            <p><span className='text-2xl  font-saira font-bold 2xl:text-3xl'>1% </span> Further development of WebAI ecosystem</p>
+                            <p><span className='text-2xl  font-saira font-bold 2xl:text-3xl'>2% </span> Strategic marketing of WebAI token and ecosystem</p>
+                        </div> 
+                        </div>
+                        
+                    </div>
+                </div>
+                <div className='max-h-[25rem] w-fullflex items-center justify-between mx-auto lg:flex-row flex-col gap-y-10'>
                         <div className='max-w-[30rem] h-full w-full flex items-center justify-center mx-auto mt-10'>
                             <Barchart data={data} options={options} />
                         </div>
                     </div>
-                </section>
+
+                <motion.img initial={{ scale: 0.5, opacity: 0 }} animate={inView ? { scale: 1, opacity: 100, y: [120, 0] } : {}} transition={{ duration: 0.7 }} src="/man.webp" alt="man" className=" absolute bottom-0 right-0 z-0 md:block hidden" ref={ref} />
+                <img src="/token.webp" alt="token" className=" absolute left-0 w-[35vh] my-auto top-0 bottom-0 ml-2" />
+
+                
 
             </section>
 
