04821302ba71e54d39079cc9848b9ddd62fe9afb
KoichiDevs <125679139+KoichiDevs@users.noreply.github.com>
2023-02-27 03:09:01 +0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,18 +28,32 @@ const Hero = () => {
                                 </div>
 
                             </motion.div>
+                            <motion.div initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.05 }} className='flex lg:text-xl text-xl font-bold mt-6 font-saira gap-x-[7px] mx-auto w-fit lg:mx-0 lg:w-auto min-w-[22rem] items-center justify-center lg:justify-start'>
+                                <h2 className='hidden'>Contract Address</h2>
+                                <div className='text-header'>
+                                    <Typewriter
+                                        options={{
+                                            strings: ['0x3Bc0f4F178666311F51e028FC3C1d5b561cB1dF6'],
+                                            autoStart: true,
+                                            loop: true,
+                                        }}
+                                    />
+                                </div>
 
+                            </motion.div>
                             <motion.p initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.1 }} className='mt-7 xl:max-w-[40rem] lg:max-w-[30rem] max-w-[40rem] font-poppins lg:text-md text-sm px-10 lg:px-0 2xl:text-lg'>AI technology is revolutionizing the world in exciting and groundbreaking ways. Artificial Intelligence 	&#40;AI&#41; is a form of technology that uses algorithms, advanced software and powerful computers to process and interpret data like humans. AI has the potential to dramatically change our society as we know it, providing solutions to problems never before thought possible.</motion.p>
 
                             <div className='flex gap-x-3 flex-col lg:flex-row items-center justify-center lg:justify-start gap-y-5'>
                                 <motion.button initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.15 }} className='bg-button px-12 py-3 w-fit h-fit rounded-lg mt-10 hover:bg-[#040B9E] cursor-pointer z-10 relative transition-colors ease-in-out duration-300 mx-auto lg:mx-0 font-medium'>
-                                    <a href="https://pancakeswap.finance/swap?outputCurrency=0x3Bc0f4F178666311F51e028FC3C1d5b561cB1dF6"  target="_blank">Buy Token</a>
+                                    <p>Buy Token</p>
                                 </motion.button>
 
                                 <motion.button initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.15 }} className='border-[1px] border-white px-12 py-3 w-fit h-fit rounded-lg lg:mt-10 hover:bg-white hover:text-black cursor-pointer z-10 relative transition-colors ease-in-out duration-300 mx-auto lg:mx-0 font-medium'>
-                                    <a href="https://www.dexview.com/bsc/0x3Bc0f4F178666311F51e028FC3C1d5b561cB1dF6" target="_blank">Chart</a>
+                                    <p>Chart</p>
                                 </motion.button>
                             </div>
+
+                           
 
                         </div>
                     </div>
