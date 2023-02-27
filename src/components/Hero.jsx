8c4d21010609eb2fc96a57917ad1c5e1f5196a42
KoichiDevs <alexisken1432@gmail.com>
2023-02-27 12:50:47 +0800

    added copy

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Parallax } from 'react-scroll-parallax'
 import Nav from '../utils/Nav'
 import Typewriter from "typewriter-effect"
 import { motion } from 'framer-motion'
+import { MdCopyAll } from 'react-icons/md'
 
 const Hero = () => {
+
+    const [contactAddress, setContact] = useState('0x3Bc0f4F178666311F51e028FC3C1d5b561cB1dF6')
+    const [clicked, setClicked] = useState(false)
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(contactAddress)
+        setClicked(true)
+
+        setTimeout(() => {
+            setClicked(false)
+
+        }, 1000);
+
+    }
 
     return (
         <>
@@ -14,7 +29,7 @@ const Hero = () => {
 
                     <div className='w-full max-w-[1700px] mx-auto flex items-center h-full lg:px-20 z-10 relative text-center lg:text-left justify-center lg:justify-start'>
                         <div className=''>
-                            <motion.h1 initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1 }} className='font-saira lg:text-8xl font-black text-5xl'>WEB AI</motion.h1>
+                            <motion.h1 initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1 }} className='font-saira lg:text-8xl font-black text-4xl'>WEB AI</motion.h1>
                             <motion.div initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.05 }} className='flex lg:text-3xl text-xl font-bold font-saira gap-x-[7px] mx-auto w-fit lg:mx-0 lg:w-auto min-w-[22rem] items-center justify-center lg:justify-start'>
                                 <h2 className=''>The future of</h2>
                                 <div className='text-header'>
@@ -28,32 +43,41 @@ const Hero = () => {
                                 </div>
 
                             </motion.div>
-                            <motion.div initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.05 }} className='flex lg:text-xl text-xl font-bold mt-6 font-saira gap-x-[7px] mx-auto w-fit lg:mx-0 lg:w-auto min-w-[22rem] items-center justify-center lg:justify-start'>
-                                <h2 className='hidden'>Contract Address</h2>
-                                <div className='text-header'>
-                                    <Typewriter
-                                        options={{
-                                            strings: ['0x3Bc0f4F178666311F51e028FC3C1d5b561cB1dF6'],
-                                            autoStart: true,
-                                            loop: true,
-                                        }}
-                                    />
-                                </div>
 
-                            </motion.div>
-                            <motion.p initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.1 }} className='mt-7 xl:max-w-[40rem] lg:max-w-[30rem] max-w-[40rem] font-poppins lg:text-md text-sm px-10 lg:px-0 2xl:text-lg'>AI technology is revolutionizing the world in exciting and groundbreaking ways. Artificial Intelligence 	&#40;AI&#41; is a form of technology that uses algorithms, advanced software and powerful computers to process and interpret data like humans. AI has the potential to dramatically change our society as we know it, providing solutions to problems never before thought possible.</motion.p>
+                            <motion.p initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.1 }} className='mt-7 xl:max-w-[40rem] lg:max-w-[30rem] max-w-[40rem] font-poppins lg:text-md sm:text-sm text-xs px-10 lg:px-0 2xl:text-lg'>AI technology is revolutionizing the world in exciting and groundbreaking ways. Artificial Intelligence 	&#40;AI&#41; is a form of technology that uses algorithms, advanced software and powerful computers to process and interpret data like humans. AI has the potential to dramatically change our society as we know it, providing solutions to problems never before thought possible.</motion.p>
 
                             <div className='flex gap-x-3 flex-col lg:flex-row items-center justify-center lg:justify-start gap-y-5'>
                                 <motion.button initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.15 }} className='bg-button px-12 py-3 w-fit h-fit rounded-lg mt-10 hover:bg-[#040B9E] cursor-pointer z-10 relative transition-colors ease-in-out duration-300 mx-auto lg:mx-0 font-medium'>
                                     <p>Buy Token</p>
                                 </motion.button>
 
+
+
                                 <motion.button initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.15 }} className='border-[1px] border-white px-12 py-3 w-fit h-fit rounded-lg lg:mt-10 hover:bg-white hover:text-black cursor-pointer z-10 relative transition-colors ease-in-out duration-300 mx-auto lg:mx-0 font-medium'>
                                     <p>Chart</p>
                                 </motion.button>
+
+                                <motion.button initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.15 }} className='border-[1px] border-white px-8 py-3 w-fit h-fit rounded-lg lg:mt-10 hover:bg-white hover:text-black cursor-pointer z-10 relative transition-colors ease-in-out duration-300 mx-auto lg:mx-0 font-medium flex items-center gap-x-2' onClick={handleCopy}>
+                                    <MdCopyAll className='text-xl block' stle={clicked ? {display: "hidden"} : {}}/>
+                                    <p>{clicked ? "Copied!" : "Address"}</p>
+                                </motion.button>
                             </div>
 
-                           
+                            <motion.div initial={{ x: -200, opacity: 0 }} animate={{ x: 0, opacity: 100 }} transition={{ duration: 1, delay: 0.05 }} className='lg:text-xl text-xl font-bold mt-6 font-saira gap-x-[7px] mx-auto w-fit lg:mx-0 lg:w-auto min-w-[22rem] items-center justify-center lg:justify-start lg:flex hidden'>
+                                <h2 className='hidden'>Contract Address</h2>
+                                <div className='text-header'>
+                                    <Typewriter
+                                        options={{
+                                            strings: [contactAddress],
+                                            autoStart: true,
+                                            loop: true,
+                                        }}
+                                    />
+                                </div>
+                            </motion.div>
+
+
+
 
                         </div>
                     </div>
