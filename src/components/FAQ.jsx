d2aaf44e5ba30caf9483bfee4567e7a775fbf07f
KoichiDevs <125679139+KoichiDevs@users.noreply.github.com>
2023-03-02 00:11:30 +0800

    Update FAQ.jsx

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -9,7 +9,7 @@ const FAQ = () => {
 
                 <Question title="Q1: What are the safety measures for the project?" desc="Three main components making the project safe are KYC, Audit and DOXX features which will be done through Pinksale launchpad platform. Audit will be implemented for $WEBAI token contract in order to guarantee safety of the trading for investors." />
 
-                <Question title="Q2: Where I can buy $WEBAI?" desc="From 22nd to 24th February, Web AI is conducting a fairlaunch on Pinksale platform where investors are able to contribute before $WEBAI is officially launched. After 24th February, next chance to buy $WEBAI tokens is the official launch day, 27th February, where trading opens on Pancakeswap DEX. “BUY” button will appear our website in order to provide easy access to Pancakeswap page." />
+                <Question title="Q2: Where I can buy $WEBAI?" desc="From 27th February to 1st March, Web AI is conducting a fairlaunch on Pinksale platform where investors are able to contribute before $WEBAI is officially launched. After 24th February, next chance to buy $WEBAI tokens is the official launch day, 27th February, where trading opens on Pancakeswap DEX. “BUY” button will appear our website in order to provide easy access to Pancakeswap page. trading opens up on Bitmart and Pancakeswap" />
 
                 <Question title="Q3: How I can contact Web AI team?" desc="To contact us, feel free to join our Telegram community at @WebAIofficial where you are able to ask any questions regarding Web AI project. For any sort of proposals, we have instructions in our Telegram community to hear your thoughts about the project." />
 
